b65ab07d1f2309679666fac615bee21ca7662327
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-02-08 05:27:15 -0800

    fix Pep8 validation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-
-"""Rectangle class"""
-
-
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
-class Square(Rectangle):
-     """Represent a square."""
 
-     def __init__(self, size):
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
         """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
         """
-        super().integer_validator("size", size)
-        self.__size = size
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
