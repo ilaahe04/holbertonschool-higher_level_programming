1b5cd6dea80ee582b4d63c83ea4476c39c3b655b
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-02-08 05:21:22 -0800

    Square #1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Rectangle class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+     """Represent a square."""
+
+     def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
