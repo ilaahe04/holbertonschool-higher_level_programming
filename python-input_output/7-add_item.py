8f31068d84c5af6f97d7791e8b022a292dd0d443
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-02-09 04:37:18 -0800

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,7 +8,6 @@
 
 import json
 import sys
-
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
@@ -16,4 +15,4 @@ try:
     txt = load_from_json_file("add_item.json")
 except Exception:
     txt = []
-save_to_json_file(loads + sys.argv[1:], "add_item.json")
+save_to_json_file(txt + sys.argv[1:], "add_item.json")
