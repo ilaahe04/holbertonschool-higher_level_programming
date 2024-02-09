78bc5299749a82dd8ed2047990627e22493d8266
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-02-09 09:44:22 -0800

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""json"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new = {}
+        if attrs is not None and all(isinstance(item, str) for item in attrs):
+            for i in attrs:
+                try:
+                    new[i] = self.__dict__[i]
+                except KeyError:
+                    pass
+            return new
+        return self.__dict__
