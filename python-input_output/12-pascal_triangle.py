e9ba9c5dba4ff4508319c8b4c10fbf9b25d82052
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-02-09 09:58:11 -0800

    12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""pascal triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return list
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(triangle[i - 1]) - 1):
+            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+        temp.append(1)
+        triangle.append(temp)
+    return triangle
