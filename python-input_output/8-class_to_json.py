af202b4070b5556d7cacc2160de806e7ab417daa
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-02-09 09:06:36 -0800

    Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/python3
+"""json"""
+
+
+def class_to_json(obj):
+     """Initialize an empty dictionary"""
+    j_dict = {}
+
+    for attr, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            j_dict[attr] = value
+
+    return j_dict
+
