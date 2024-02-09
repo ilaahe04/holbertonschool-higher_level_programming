9572a3b8fc0bb65349554379548f4caabd1b7fce
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-02-08 21:15:18 -0800

    Append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
+def append_write(filename="", text=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, "a", encoding="utf-8") as f:
+        txt = f.write(text)
+        return txt
