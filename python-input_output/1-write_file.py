59d1fd34de8544b3c784eb9791a1c2e7a0fbf483
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-02-08 21:09:31 -0800

    write to file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
+def write_file(filename="", text=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, "w", encoding="utf-8") as f:
+        txt = f.write(text)
+        return txt
