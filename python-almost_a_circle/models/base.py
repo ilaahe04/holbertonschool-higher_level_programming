cb3d2facfa119ca3db00c39d36232186e7c491c2
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-02-13 21:46:02 -0800

    Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Class Base"""
+import json
 
 
 class Base:
@@ -12,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Update the class Base"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
