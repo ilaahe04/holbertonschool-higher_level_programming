049497238ea3a50a55dbf90e2d946467ca138a02
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-03-23 10:01:36 +0000

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Lists all states with names starting with 'N'
-from the database hbtn_0e_0_usa."""
+"""lists all states with a name starting with N
+from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
+import sqlalchemy
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-    for state in cur.fetchall():
-        print(state)
-    cur.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    [print(state) for state in cursor.fetchall() if state[1][0] == "N"]
