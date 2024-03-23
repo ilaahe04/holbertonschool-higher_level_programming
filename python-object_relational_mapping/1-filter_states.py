e9e3ad0e9136573e3fb6d0b7c527455da35d3c0e
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-03-23 09:58:39 +0000

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ import sqlalchemy
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     for state in cur.fetchall():
         print(state)
     cur.close()
