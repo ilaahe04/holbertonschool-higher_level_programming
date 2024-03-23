36da0bc976d7afb6c8d3877a5ff1ec2d008ba319
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-03-23 10:14:52 +0000

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Connectio to database"""
-
+"""Connect to database"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
+    query = "SELECT * FROM states WHERE BINARY name = '{}'".format(sys.argv[4])
     cur.execute(query)
     for state in cur.fetchall():
         print(state)
