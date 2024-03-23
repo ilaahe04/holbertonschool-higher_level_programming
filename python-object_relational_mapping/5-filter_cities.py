10a3b91cc9af9c59a05b0ca174c9dd14cbf41c17
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-03-23 10:43:20 +0000

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,9 +5,11 @@ Script to list all cities of a given state from the database hbtn_0e_4_usa.
 
 import MySQLdb
 import sys
+import sqlalchemy
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
     cur.execute("SELECT * \
             FROM cities \
             INNER JOIN states \
