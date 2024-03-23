4bfe7a6d223c5ea81487247a8437cb6b048cc3f0
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-03-23 10:00:07 +0000

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Lists all states with names starting with 'N' from the database hbtn_0e_0_usa."""
+"""Lists all states with names starting with 'N'
+from the database hbtn_0e_0_usa."""
 
 import MySQLdb
 import sys
-import sqlalchemy
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
